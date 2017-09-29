dd7503fcf73b30dee9bf4914815e28b4091b01c4
Michał Czaplicki <mcz.czaplicki@gmail.com>
2017-09-30 01:40:43 +0200

    intro almost ready, need some improvmed in link plus text

--- assets/js/hide_and_seak.js
+++ assets/js/hide_and_seak.js
@@ -21,5 +21,5 @@ function show0(){
     document.getElementById('1').style.display ='none';
     document.getElementById('2').style.display ='none';
     document.getElementById('3').style.display ='none';
-    document.getElementById('intro').style.display ='block';
+    document.getElementById('intro').style.display ='grid';
 }
